eeacd13d08cee0e54b1a032728e581574f0454ac
Chough <carter.hough@hotmail.com>
2016-10-10 14:05:20 -0600

    Initial commit. Set up primary project folder, and kiss library.

---
+++ Java_Class/src/java_class/Java_Class.java
@@ -0,0 +1,20 @@
+/*
+ * Carter Hough
+ * CSCI 310 - Java
+ * Dr. MacEvoy
+ * 
+ * Java_Class.java
+ */
+
+// Main project folder for CSCI 310 - Java. Each assignment is in a different
+//  .java file, and under its own branch in the Git repository.
+
+package java_class;
+
+public class Java_Class 
+{
+    public static void main(String[] args) 
+    {
+
+    }
+}
