e89a0890855bb162a2c2d6b6f88fea026da9cb2d
Chough <carter.hough@hotmail.com>
2016-10-10 14:16:13 -0600

    Created file for Assignment 2, and created new branch.

---
+++ Java_Class/src/java_class/Assignment2_Collections.java
@@ -0,0 +1,16 @@
+/*
+ * Carter Hough
+ * 10.10.16
+ * CSCI 310 - Java
+ * Assignment 2 - Collections
+ * Dr. MacEvoy
+ * 
+ * Assignment2_Collections.java
+ */
+
+package java_class;
+
+public class Assignment2_Collections 
+{
+    
+}
