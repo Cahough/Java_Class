84251782a51084f229cc9e11aaf4652f0bec28af
Chough <carter.hough@hotmail.com>
2016-10-31 14:47:07 -0600

    Trying to fix NetBeans Project & Git Repo.

---
+++ src/java_class/Java_Class.java
@@ -0,0 +1,19 @@
+/*
+ * Carter Hough
+ * Date
+ * Course
+ * Project
+ * Professor
+ * 
+ * file.java
+ */
+
+package java_class;
+
+public class Java_Class 
+{
+    public static void main(String[] args) 
+    {
+
+    }
+}
