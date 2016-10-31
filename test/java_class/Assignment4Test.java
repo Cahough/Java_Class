6252f2b35e15f237daa73680290084b1092f4e65
Chough <carter.hough@hotmail.com>
2016-10-31 15:07:39 -0600

    Added JUnit and auto-tests inside Assignment4Test.java

---
+++ test/java_class/Assignment4Test.java
@@ -0,0 +1,57 @@
+/*
+ * Carter Hough
+ * 10-31-16
+ * CSCI 310 - Java
+ * Assignment 4 - JUnit Testing
+ * Dr. MacEvoy
+ * 
+ * Assignment4Test.java
+ */
+
+package java_class;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author Chough
+ */
+
+public class Assignment4Test {
+    
+    public Assignment4Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Assignment4.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Assignment4.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
