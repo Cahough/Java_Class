3309068dcc64b59decbd8c17c88c9c66b4a2bca7
Chough <carter.hough@hotmail.com>
2016-10-31 15:31:56 -0600

    More testing

--- test/java_class/Assignment4Test.java
+++ test/java_class/Assignment4Test.java
@@ -11,10 +11,6 @@
 package java_class;
 
 import java.util.*;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -22,35 +18,47 @@ import static org.junit.Assert.*;
  * @author Chough
  */
 
-public class Assignment4Test {
-    
-    public Assignment4Test() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
+public class Assignment4Test 
+{
     /**
      * Test of main method, of class Assignment4.
      */
+    
     @Test
-    public void testMain() {
+    public void testMain() 
+    {
         System.out.println("main");
         String[] args = null;
         Assignment4.main(args);
+        
+        ArrayList[] set = new ArrayList[5];
+        set[0] = new ArrayList(5);
+        set[0].add(41);
+        set[0].add(77);
+        set[0].add(74);
+        set[0].add(22);
+        set[0].add(44);
+        
+        set[1] = new ArrayList(1);
+        set[1].add(12);
+        
+        set[2] = new ArrayList(4);
+        set[2].add(37);
+        set[2].add(34);
+        set[2].add(36);
+        set[2].add(52);
+        
+        set[3] = new ArrayList(0);
+        
+        set[4] = new ArrayList(3);
+        set[4].add(20);
+        set[4].add(22);
+        set[4].add(33);
+        
+        assert set[0].get(4).equals(44);
+        assert set[1].get(0).equals(12);
+        assert set[2].get(2).equals(36);
+        assert set[3].isEmpty();
+        assert set[4].get(0).equals(20);
     }
-    
 }
