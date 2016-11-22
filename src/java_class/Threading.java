5d43d22d66f3ce38a860cd2d9efe064760e7b3cd
Chough <carter.hough@hotmail.com>
2016-11-21 20:07:10 -0700

    Added/implemented Threading Class as well as main method. Had to re-name file as well as original class.

---
+++ src/java_class/Threading.java
@@ -0,0 +1,63 @@
+/*
+ * Carter Hough
+ * 11.21.16
+ * CSCI 310 - Java
+ * Assignment #5 - MultiThreading
+ * Dr. MacEvoy
+ * 
+ * Threading.java
+ */
+
+package java_class;
+
+class Runner implements Runnable
+{
+    private Thread t;
+    private String threadName;
+    
+    Runner(String name)
+    {
+        threadName = name;
+        System.out.println("Creating " + threadName);
+    }
+    
+    @Override
+    public void run()
+    {
+        System.out.println("Running" + threadName);
+        try
+        {
+            for (int i = 4; i >0; i--)
+            {
+                System.out.println("Thread: " + threadName + ", " + i);
+                Thread.sleep(50);
+            }
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Thread: " + threadName + " interrupted.");
+        }
+        System.out.println("Thread: " + threadName + " exiting.");
+    }
+    
+    public void start()
+    {
+        System.out.println("Starting " + threadName);
+        if (t == null)
+        {
+            t = new Thread (this, threadName);
+            t.start();
+        }
+    }
+}
+
+public class Threading
+{
+    public static void main(String args[])
+    {
+        Runner R1 = new Runner("Thread-1");
+        R1.start();
+        Runner R2 = new Runner("Thread-2");
+        R2.start();
+    }
+}
