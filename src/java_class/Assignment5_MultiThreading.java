41f60194861e7a7c056787fe524f3f69e871ce52
Chough <carter.hough@hotmail.com>
2016-11-21 19:44:56 -0700

    Initial commit for Assignment #5. Setup file for multithreading.

---
+++ src/java_class/Assignment5_MultiThreading.java
@@ -0,0 +1,19 @@
+/*
+ * Carter Hough
+ * 11.21.16
+ * CSCI 310 - Java
+ * Assignment #5 - MultiThreading
+ * Dr. MacEvoy
+ * 
+ * Assignment5_MultiThreading.java
+ */
+
+package java_class;
+
+public class Assignment5_MultiThreading 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
