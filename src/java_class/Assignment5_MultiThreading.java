d1c7108325b9017c11f4caead14119b768c07ed6
Chough <carter.hough@hotmail.com>
2016-11-21 19:56:25 -0700

    Added/implemented run class.

--- src/java_class/Assignment5_MultiThreading.java
+++ src/java_class/Assignment5_MultiThreading.java
@@ -10,10 +10,33 @@
 
 package java_class;
 
-public class Assignment5_MultiThreading 
+public class Assignment5_MultiThreading implements Runnable
 {
-    public static void main(String[] args) 
+    private Thread t;
+    private String threadName;
+    
+    Assignment5_MultiThreading(String name)
     {
-        
+        threadName = name;
+        System.out.println("Creating " + threadName);
+    }
+    
+    @Override
+    public void run()
+    {
+        System.out.println("Running" + threadName);
+        try
+        {
+            for (int i = 4; i >0; i--)
+            {
+                System.out.println("Thread: " + threadName + ", " + i);
+                Thread.sleep(50);
+            }
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Thread: " + threadName + " interrupted.");
+        }
+        System.out.println("Thread: " + threadName + " exiting.");
     }
 }
