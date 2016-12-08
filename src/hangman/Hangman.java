fe1522b46ddbb4d2a760a7fdc577336f45bdeea2
Chough <carter.hough@hotmail.com>
2016-12-08 11:49:37 -0700

    Initial commit. Setup files and code for Hangman.java and main class. Code is commented out until other code from views and models has been implemented.

---
+++ src/hangman/Hangman.java
@@ -0,0 +1,29 @@
+/*
+ * Carter Hough
+ * 12.8.16
+ * CSCI 310: Java
+ * Final Project - Hangman
+ * Dr. MacEvoy
+ * 
+ * Hangman.java
+ */
+
+package hangman;
+
+import javax.swing.SwingUtilities;
+// import hangman.model.hangmanModel;
+// import hangman.view.hangmanFrame
+
+//public class Hangman implements Runnable
+//{
+//    @Override
+//    public void run()
+//    {
+//        new hangmanFrame(new hangmanModel());
+//    }
+//    
+//    public static void main(String[] args) 
+//    {
+//        SwingUtilities.invokeLater(new Hangman());
+//    }
+//}
